d9a17a94c0fe24b66b717a335cdf267c6793d1ed
deus_mac1 <deus_mac1@deus-mac1s-MBP.lan>
2024-04-12 13:12:25 +0100

    Change template actions to allow cy envs to be used on same test

--- src/parse/actions.py
+++ src/parse/actions.py
@@ -10,12 +10,16 @@ def parser(actions):
     res_locators = resolve_locators(locators)
     action_renderer = ActionRenderer()
 
-    for action in actions:
+    for index, action in enumerate(actions):
         action_name = next(iter(action)) if type(action) is not str else action
         action_args = action[next(iter(action))] if type(action) is not str else {}
 
         rendered_action = "\t\t" + action_renderer.render_action(action_name, action_args, res_locators)
 
+        if index == 0: 
+            rendered_action = rendered_action.replace(".then(() => {", "", 1).rsplit("})", 1)[0]
+            rendered_action = rendered_action.replace("\t", "", 1) + "\n"
+        
         rendered_action_list = rendered_action_list + rendered_action
 
         if "throw new Error" not in rendered_action_list: continue 
