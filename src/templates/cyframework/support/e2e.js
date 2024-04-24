2c09a0885e2b6fccdb48bf16d2d79d11b55b22dc
JoaoFernandes <deus_mac1@deus-mac1s-MBP.lan>
2024-04-24 19:30:38 +0100

    Update: removed verbosity of console logs for test runs

--- src/templates/cyframework/support/e2e.js
+++ src/templates/cyframework/support/e2e.js
@@ -1,10 +1,10 @@
 import 'cypress-plugin-xhr-toggle'
 import 'cypress-xpath'
-require('cypress-failed-log')
-import "cypress-failed-log"
+// require('cypress-failed-log')
+// import "cypress-failed-log"
 const options = {
     // Log console output only
-    collectTypes: ['cons:log', 'cons:error', 'cy:log', 'cy:request', 'cy:intercept', 'cy:command'],
+    collectTypes: ['cy:log'],
   };
 require('cypress-terminal-report/src/installLogsCollector')(options);
 
